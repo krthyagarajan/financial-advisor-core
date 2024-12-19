17f0662774293967b3d64debb144ab876f2a0475
Thyagarajan <thyagarajan1234@gmail.com>
2024-12-19 16:14:50 +0530

    Applicatio skeleton

---
+++ src/test/java/com/advisor/financial_advisor/FinancialAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advisor.financial_advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
